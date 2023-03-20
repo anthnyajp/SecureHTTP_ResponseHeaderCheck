f0b7168c5d33f79285071cb0989f32c5e65e5e67
Anthony Pinto <anthnyajp@aol.com>
2023-03-20 16:44:43 -0400

    Update URLstatusAndHeaders.ps1

--- URLstatusAndHeaders.ps1
+++ URLstatusAndHeaders.ps1
@@ -2,8 +2,7 @@ CLS
 
 # Need to ADD or Fix
     # Check for Insecure Redirect
-    # Check sourece code for HTTP Links / Mixed Content
-    # Checking each response cookie for secure & https
+     # Checking each response cookie for secure & https
 
 #-------------
 # Script to loop over a list of URLs and export security response headers to csv.
@@ -14,6 +13,24 @@ CLS
 #               - StatusCode = response status code (blank if error code!)
 #               - Error = Error message
 #
+<#
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+
+#>
 
 add-type @"
     using System.Net;
@@ -26,22 +43,22 @@ add-type @"
         }
     }
 "@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
 $Date= ((Get-Date).ToString("yyyyMMdd_HHmmss"))
 
 # Update paths to import file and export directory if needed
-$linksFilePath = "C:\Downloads\WebURLs.txt"
-$outCSVPath = "C:\Downloads\URL_Header_Checker_Export_"+$Date+".csv"
-
-#use this to import and export file from same directory as script
-#$linksFilePath = "$PSScriptRoot\WebURLs.txt"
-#$outCSVPath = "$PSScriptRoot\URL_Header_Checker_Export_"+$Date+".csv"
+#$linksFilePath = "C:\Downloads\WebURLs.txt"
+#$outCSVPath = "C:\Downloads\URL_Header_Checker_Export_"+$Date+".csv"
+$linksFilePath = "$PSScriptRoot\WebURLs.txt"
+$outCSVPath = "$PSScriptRoot\URL_Header_Checker_Export_"+$Date+".csv"
 
 get-content $linksFilePath |
  Foreach { $uril = $_; try 
     { 
-        Invoke-WebRequest -Uri $uril -Method GET -MaximumRedirection 10 -ErrorAction SilentlyContinue -UseBasicParsing 
+        Invoke-WebRequest -Uri $uril -Method GET -UseBasicParsing -MaximumRedirection 10 -ErrorAction SilentlyContinue 
 
     } 
         catch [System.Net.WebException]
@@ -52,6 +69,6 @@ get-content $linksFilePath |
 
     } |
     
- Select @{Name="RequestURI";Expression={$uril}},StatusCode, StatusDescription , Error , @{Name="Final_URL";Expression={$_.BaseResponse.ResponseUri.AbsoluteUri}},@{Name="Server";Expression={$_.Headers["Server"]}} ,  @{Name="CSP";Expression={$_.Headers["Content-Security-Policy"]}}  ,@{Name="X-Content-Type-Options";Expression={$_.Headers["X-Content-Type-Options"]}} , @{Name="Strict-Transport-Security";Expression={$_.Headers["Strict-Transport-Security"]}} ,  @{Name="Cache-Control";Expression={$_.Headers["Cache-Control"]}} ,  @{Name="Expires";Expression={$_.Headers["Expires"]}} , @{Name="Set-Cookie";Expression={$_.Headers["Set-Cookie"]}} ,@{Name="X-XSS-Protection";Expression={$_.Headers["X-XSS-Protection"]}} ,   @{Name="X-Frame-Options";Expression={$_.Headers["X-Frame-Options"]}} , @{Name="Referrer-Policy";Expression={$_.Headers["Referrer-Policy"]}} , @{Name="Clear-Site-Data";Expression={$_.Headers["Clear-Site-Data"]}} , @{Name="Feature-Policy";Expression={$_.Headers["Feature-Policy"]}} ,    @{Name="Expect-CT";Expression={$_.Headers["Expect-CT"]}} , @{Name="X-Permitted-Cross-Domain-Policies";Expression={$_.Headers["X-Permitted-Cross-Domain-Policies"]}} , @{Name="Cross-Origin-Embedder-Policy";Expression={$_.Headers["Cross-Origin-Embedder-Policy"]}} ,     @{Name="Cross-Origin-Opener-Policy";Expression={$_.Headers["Cross-Origin-Opener-Policy"]}} , @{Name="Cross-Origin-Resource-Policy";Expression={$_.Headers["Cross-Origin-Resource-Policy"]}} , @{Name="Links";Expression={[string]$_.links.href -like "http:*"}}|
+ Select @{Name="RequestURI";Expression={$uril}},StatusCode, StatusDescription , Error , @{Name="ProtocolVersion";Expression={$_.BaseResponse.ProtocolVersion}} , @{Name="Final_URL";Expression={$_.BaseResponse.ResponseUri.AbsoluteUri}},@{Name="Server";Expression={$_.Headers["Server"]}}, @{Name="Content-Type";Expression={$_.Headers["Content-Type"]}} ,  @{Name="CSP";Expression={$_.Headers["Content-Security-Policy"]}}  ,@{Name="X-Content-Type-Options";Expression={$_.Headers["X-Content-Type-Options"]}} , @{Name="Strict-Transport-Security 31536000";Expression={$_.Headers["Strict-Transport-Security"]}} ,  @{Name="Cache-Control";Expression={$_.Headers["Cache-Control"]}} ,  @{Name="Expires";Expression={$_.Headers["Expires"]}} , @{Name="Set-Cookie";Expression={$_.Headers["Set-Cookie"]}} ,@{Name="X-XSS-Protection";Expression={$_.Headers["X-XSS-Protection"]}} ,  @{Name="X-Frame-Options";Expression={$_.Headers["X-Frame-Options"]}} , @{Name="Access-Control-Allow-Origin";Expression={$_.Headers["Access-Control-Allow-Origin"]}} , @{Name="Referrer-Policy";Expression={$_.Headers["Referrer-Policy"]}} , @{Name="Permission-Policy";Expression={$_.Headers["Permission-Policy"]}} , @{Name="Clear-Site-Data";Expression={$_.Headers["Clear-Site-Data"]}} , @{Name="Feature-Policy";Expression={$_.Headers["Feature-Policy"]}} , @{Name="Expect-CT";Expression={$_.Headers["Expect-CT"]}} , @{Name="X-Permitted-Cross-Domain-Policies";Expression={$_.Headers["X-Permitted-Cross-Domain-Policies"]}} , @{Name="Cross-Origin-Embedder-Policy";Expression={$_.Headers["Cross-Origin-Embedder-Policy"]}} , @{Name="Cross-Origin-Opener-Policy";Expression={$_.Headers["Cross-Origin-Opener-Policy"]}} , @{Name="Cross-Origin-Resource-Policy";Expression={$_.Headers["Cross-Origin-Resource-Policy"]}} , @{Name="Links";Expression={[string]$_.links.href -like "http:*"}}|
 
  Export-Csv $outCSVPath -NoTypeInformation
